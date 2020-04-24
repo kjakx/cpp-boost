27b6334c2e9d7a493b2e3cd8e973b0ba55ee23e4
kjakx <fxhfr163@ybb.ne.jp>
2020-04-24 11:27:28 +0000

    dentaku3

--- playground.cpp
+++ playground.cpp
@@ -1,41 +1,309 @@
 #include <bits/stdc++.h>
+#include <stdexcept>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+// 問題文の形式でvec値を出力
+void print_vec(vector<int> vec) {
+  cout << "[ ";
+  for (int i = 0; i < vec.size(); i++) {
+    cout << vec.at(i) << " ";
+  }
+  cout << "]" << endl;
+}
+
+void add_vec(vector<int>& a, vector<int>& b)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] += b[i];
+    }
+}
+
+void sub_vec(vector<int>& a, vector<int>& b)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] -= b[i];
+    }
+}
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<bool>> arefriends(n, vector<bool>(n, false));
-    for (int i = 0; i < m; i++)
+    int n; cin >> n;
+    map<char, int> integer;
+    map<char, vector<int>> vec;
+    for (int i = 0; i < n; i++)
     {
-        int x, y;
-        cin >> x >> y;
-        arefriends[x-1][y-1] = true;
-        arefriends[y-1][x-1] = true;
-    }
-    int max_friends = 1;
-    for (int tmp = 0; tmp < (1 << n); tmp++)
-    {
-        bitset<12> bs(tmp);
-        int i, j;
-        int areallfriends = true;
-        for (i = 0; i < n - 1; i++)
+        string str; cin >> str;
+        if (str == "int")
         {
-            for (j = i + 1; j < n; j++)
+            char x; cin >> x;
+            integer[x] = 0;
+            char c;
+            while (true)
             {
-                if (bs.test(i) && bs.test(j))
+                cin >> c;
+                if (c == ';') break;
+                if (c == '=') continue;
+                if (c == '+')
                 {
-                    if (arefriends[i][j] == false) 
-                    {
-                        areallfriends = false;
-                        break;
-                    }
+                    cin >> c;
+                    if ('0' <= c && c <= '9') integer[x] += c - '0';
+                    else integer[x] += integer[c];
+                    continue;
+                }
+                if (c == '-')
+                {
+                    cin >> c;
+                    if ('0' <= c && c <= '9') integer[x] -= c - '0';
+                    else integer[x] -= integer[c];
+                    continue;
+                }
+                if ('0' <= c && c <= '9') 
+                {
+                    integer[x] += c - '0';
+                    continue;
+                }
+                if ('a' <= c && c <= 'z') 
+                {
+                    integer[x] += integer[c];
+                    continue;
                 }
             }
-            if (areallfriends == false) break;
         }
-        if (areallfriends == true) 
-            max_friends = max((int)bs.count(), max_friends);
+        else if (str == "vec")
+        {
+            char x; cin >> x;
+            char c;
+            while (true)
+            {
+                cin >> c;
+                if (c == ';') break;
+                if (c == '=') continue;
+                if (c == '+')
+                {
+                    cin >> c;
+                    if ('a' <= c && c <= 'z')
+                    {
+                        add_vec(vec[x], vec[c]);
+                        continue;
+                    }
+                    if (c == '[')
+                    {
+                        vector<int> tmp;
+                        while (true)
+                        {
+                            cin >> c;
+                            if (c == ']') break;
+                            if (c == ',') continue;
+                            if ('0' <= c && c <= '9') 
+                            {
+                                tmp.push_back(c - '0');
+                                continue;
+                            }
+                            if ('a' <= c && c <= 'z')
+                            {
+                                tmp.push_back(integer[c]);
+                                continue;
+                            }
+                        }
+                        add_vec(vec[x], tmp);
+                        continue;
+                    }
+                }
+                if (c == '-')
+                {
+                    cin >> c;
+                    if ('a' <= c && c <= 'z')
+                    {
+                        sub_vec(vec[x], vec[c]);
+                        continue;
+                    }
+                    if (c == '[')
+                    {
+                        vector<int> tmp;
+                        while (true)
+                        {
+                            cin >> c;
+                            if (c == ']') break;
+                            if (c == ',') continue;
+                            if ('0' <= c && c <= '9') 
+                            {
+                                tmp.push_back(c - '0');
+                                continue;
+                            }
+                            if ('a' <= c && c <= 'z')
+                            {
+                                tmp.push_back(integer[c]);
+                                continue;
+                            }
+                        }
+                        sub_vec(vec[x], tmp);
+                        continue;
+                    }
+                }
+                if (c == '[')
+                {
+                    while (true)
+                    {
+                        cin >> c;
+                        if (c == ']') break;
+                        if (c == ',') continue;
+                        if ('0' <= c && c <= '9') 
+                        {
+                            vec[x].push_back(c - '0');
+                            continue;
+                        }
+                        if ('a' <= c && c <= 'z')
+                        {
+                            vec[x].push_back(integer[c]);
+                            continue;
+                        }
+                    }
+                }
+                if ('a' <= c && c <= 'z') 
+                {
+                    vec[x] = vec[c];
+                    continue;
+                }
+            }
+        }
+        else if (str == "print_int")
+        {
+            int out = 0;
+            char c;
+            while (true)
+            {
+                cin >> c;
+                if (c == ';') break;
+                if (c == '=') continue;
+                if (c == '+')
+                {
+                    cin >> c;
+                    if ('0' <= c && c <= '9') out += c - '0';
+                    else out += integer[c];
+                    continue;
+                }
+                if (c == '-')
+                {
+                    cin >> c;
+                    if ('0' <= c && c <= '9') out -= c - '0';
+                    else out -= integer[c];
+                    continue;
+                }
+                if ('0' <= c && c <= '9') 
+                {
+                    out += c - '0';
+                    continue;
+                }
+                if ('a' <= c && c <= 'z') 
+                {
+                    out += integer[c];
+                    continue;
+                }
+            }
+            cout << out << endl;
+        }
+        else if (str == "print_vec")
+        {
+            vector<int> tmp;
+            char c;
+            while (true)
+            {
+                cin >> c;
+                if (c == ';') break;
+                if (c == '=') continue;
+                if (c == '+')
+                {
+                    cin >> c;
+                    if ('a' <= c && c <= 'z')
+                    {
+                        add_vec(tmp, vec[c]);
+                        continue;
+                    }
+                    if (c == '[')
+                    {
+                        vector<int> tmp2;
+                        while (true)
+                        {
+                            cin >> c;
+                            if (c == ']') break;
+                            if (c == ',') continue;
+                            if ('0' <= c && c <= '9') 
+                            {
+                                tmp2.push_back(c - '0');
+                                continue;
+                            }
+                            if ('a' <= c && c <= 'z')
+                            {
+                                tmp2.push_back(integer[c]);
+                                continue;
+                            }
+                        }
+                        add_vec(tmp, tmp2);
+                        continue;
+                    }
+                }
+                if (c == '-')
+                {
+                    cin >> c;
+                    if ('a' <= c && c <= 'z')
+                    {
+                        sub_vec(tmp, vec[c]);
+                        continue;
+                    }
+                    if (c == '[')
+                    {
+                        vector<int> tmp2;
+                        while (true)
+                        {
+                            cin >> c;
+                            if (c == ']') break;
+                            if (c == ',') continue;
+                            if ('0' <= c && c <= '9') 
+                            {
+                                tmp2.push_back(c - '0');
+                                continue;
+                            }
+                            if ('a' <= c && c <= 'z')
+                            {
+                                tmp2.push_back(integer[c]);
+                                continue;
+                            }
+                        }
+                        sub_vec(tmp, tmp2);
+                        continue;
+                    }
+                }
+                if (c == '[')
+                {
+                    while (true)
+                    {
+                        cin >> c;
+                        if (c == ']') break;
+                        if (c == ',') continue;
+                        if ('0' <= c && c <= '9') 
+                        {
+                            tmp.push_back(c - '0');
+                            continue;
+                        }
+                        if ('a' <= c && c <= 'z')
+                        {
+                            tmp.push_back(integer[c]);
+                            continue;
+                        }
+                    }
+                }
+                if ('a' <= c && c <= 'z') 
+                {
+                    tmp = vec[c];
+                    continue;
+                }
+            }
+            print_vec(tmp);
+        }
     }
-    cout << max_friends << endl;
 }
