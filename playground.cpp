6bc4e072342145dbc8eff0d0d32a7ac47be8b123
kjakx <fxhfr163@ybb.ne.jp>
2020-04-26 11:45:35 +0000

    darts

--- playground.cpp
+++ playground.cpp
@@ -4,35 +4,17 @@ using namespace std;
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 
-long max_under(long m, vector<long>& p)
-{
-    int max_id_under_m;
-    int left = 0; int right = p.size() - 1;
-    while (left <= right)
-    {
-        max_id_under_m = left + (right - left) / 2;
-        if (p[max_id_under_m] == m) break;
-        if (p[max_id_under_m] > m)
-        {
-            right = max_id_under_m - 1;
-        }
-        else
-        {
-            left = max_id_under_m + 1;
-        }
-    }
-    return p[max_id_under_m];
-}
-
 int main()
 {
-    long n, m; cin >> n >> m;
-    vector<long> p(n+1); p[0] = 0;
-    rep2 (i, 1, n+1) cin >> p[i];
+    long n, m;
+    cin >> n >> m;
+    vector<long> p(n + 1);
+    p[0] = 0;
+    rep2(i, 1, n + 1) cin >> p[i];
     vector<long> all_total;
-    rep(i, n+1)
+    rep(i, n + 1)
     {
-        rep(j, n+1)
+        rep(j, n + 1)
         {
             long total = p[i] + p[j];
             if (total <= m)
@@ -43,9 +25,10 @@ int main()
     long max_total = 0;
     rep(i, all_total.size())
     {
-        long total = all_total[i] + max_under(m - all_total[i], all_total);
-        if (total <= m)
-            max_total = max(total, max_total);
+        auto lb = lower_bound(all(all_total), m - all_total[i]);
+        if (m - all_total[i] < *lb) lb--;
+        long total = all_total[i] + *lb;
+        max_total = max(total, max_total);
     }
     cout << max_total << endl;
 }
