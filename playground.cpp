88a4755dc1ff1b3d1617184285f8685807565dff
kjakx <fxhfr163@ybb.ne.jp>
2020-04-29 13:06:13 +0000

    island

--- playground.cpp
+++ playground.cpp
@@ -4,49 +4,65 @@ using namespace std;
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 #define println(x) cout << x << endl
+#define Yes println("Yes")
+#define No println("No")
+#define YES println("YES")
+#define NO println("NO")
 
-int h, w;
-vector<vector<char>> grid;
+int island_area;
+vector<vector<char>> grid(10, vector<char>(10));
 
-bool isroad(int i, int j)
+bool on_grid(int i, int j)
 {
-    if (i < 0 || i >= h) return false;
-    if (j < 0 || j >= w) return false;
-    if (grid[i][j] == '#') return false;
+    if (i < 0 || i >= 10) return false;
+    if (j < 0 || j >= 10) return false;
     return true;
 }
 
-bool dfs(int i, int j)
+bool is_island(int i, int j)
 {
-    if (grid[i][j] == 'g') return true;
-    bool res = false;
-    grid[i][j] = '#';
-    if (res == false && isroad(i-1, j)) res |= dfs(i-1, j);
-    if (res == false && isroad(i, j+1)) res |= dfs(i, j+1);
-    if (res == false && isroad(i+1, j)) res |= dfs(i+1, j);
-    if (res == false && isroad(i, j-1)) res |= dfs(i, j-1);
-    //grid[i][j] = '.';
-    return res;
+    if (on_grid(i, j) == false) return false;
+    if (grid[i][j] == 'x') return false;
+    return true;
+}
+
+int dfs(int i, int j, vector<vector<bool>>& visited)
+{
+    if (visited[i][j] == true) return 0;
+    int count = 1;
+    visited[i][j] = true;
+    if (is_island(i-1, j)) count += dfs(i-1, j, visited);
+    if (is_island(i, j+1)) count += dfs(i, j+1, visited);
+    if (is_island(i+1, j)) count += dfs(i+1, j, visited);
+    if (is_island(i, j-1)) count += dfs(i, j-1, visited);
+    return count;
 }
 
 int main()
 {
-    cin >> h >> w;
-    grid.resize(h, vector<char>(w));
-    int si, sj;
-    rep(i, h)
+    island_area = 0;
+    rep(i, 10)
     {
-        rep(j, w)
+        rep(j, 10)
         {
             cin >> grid[i][j];
-            if (grid[i][j] == 's')
+            if (grid[i][j] == 'o') island_area++;
+        }
+    }
+    rep(i, 10)
+    {
+        rep(j, 10)
+        {
+            if (grid[i][j] == 'x')
             {
-                si = i;
-                sj = j;
+                vector<vector<bool>> visited(10, vector<bool>(10, false));
+                int count = dfs(i, j, visited);
+                if (count == island_area + 1)
+                {
+                    YES; return 0;
+                }
             }
         }
     }
-    bool res = dfs(si, sj);
-    if (res == true) cout << "Yes" << endl;
-    else             cout << "No"  << endl;
+    NO; return 0;
 }
