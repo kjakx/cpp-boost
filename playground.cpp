82a99c5a0327621f9416aac21223ea1ff886abd4
kjakx <fxhfr163@ybb.ne.jp>
2020-04-30 10:25:19 +0000

    baumtest

--- playground.cpp
+++ playground.cpp
@@ -9,60 +9,40 @@ using namespace std;
 #define YES println("YES")
 #define NO println("NO")
 
-int island_area;
-vector<vector<char>> grid(10, vector<char>(10));
-
-bool on_grid(int i, int j)
+bool dfs(int i, int prev, vector<vector<int>>& nbl, vector<bool>& visited)
 {
-    if (i < 0 || i >= 10) return false;
-    if (j < 0 || j >= 10) return false;
-    return true;
-}
-
-bool is_island(int i, int j)
-{
-    if (on_grid(i, j) == false) return false;
-    if (grid[i][j] == 'x') return false;
-    return true;
-}
-
-int dfs(int i, int j, vector<vector<bool>>& visited)
-{
-    if (visited[i][j] == true) return 0;
-    int count = 1;
-    visited[i][j] = true;
-    if (is_island(i-1, j)) count += dfs(i-1, j, visited);
-    if (is_island(i, j+1)) count += dfs(i, j+1, visited);
-    if (is_island(i+1, j)) count += dfs(i+1, j, visited);
-    if (is_island(i, j-1)) count += dfs(i, j-1, visited);
-    return count;
+    if (visited[i] == true) return true;
+    bool has_loop = false;
+    visited[i] = true;
+    for (int j : nbl[i])
+    {
+        if (j != prev)
+        {
+            has_loop |= dfs(j, i, nbl, visited);
+        }
+    }
+    return has_loop;
 }
 
 int main()
 {
-    island_area = 0;
-    rep(i, 10)
+    int n, m; cin >> n >> m;
+    vector<vector<int>> nbl(n);
+    rep(i, m)
     {
-        rep(j, 10)
+        int u, v; cin >> u >> v;
+        nbl[u-1].push_back(v-1);
+        nbl[v-1].push_back(u-1);
+    }
+    vector<bool> visited(n, false);
+    int count = 0;
+    rep(i, n)
+    {
+        if (visited[i] == false)
         {
-            cin >> grid[i][j];
-            if (grid[i][j] == 'o') island_area++;
+            bool has_loop = dfs(i, -1, nbl, visited);
+            if (has_loop == false) count++;
         }
     }
-    rep(i, 10)
-    {
-        rep(j, 10)
-        {
-            if (grid[i][j] == 'x')
-            {
-                vector<vector<bool>> visited(10, vector<bool>(10, false));
-                int count = dfs(i, j, visited);
-                if (count == island_area + 1)
-                {
-                    YES; return 0;
-                }
-            }
-        }
-    }
-    NO; return 0;
+    cout << count << endl;
 }
