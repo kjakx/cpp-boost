ecb2d9482bcf76553873974e3660575eb410a8b8
kjakx <fxhfr163@ybb.ne.jp>
2020-05-01 11:32:47 +0000

    bfs

--- playground.cpp
+++ playground.cpp
@@ -4,45 +4,79 @@ using namespace std;
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define all(v) v.begin(), v.end()
 #define println(x) cout << x << endl
+#define fi first
+#define se second
 #define Yes println("Yes")
 #define No println("No")
 #define YES println("YES")
 #define NO println("NO")
+const int di[8] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dj[8] = {0, 1, 0, -1, -1, 1, 1, -1};
 
-bool dfs(int i, int prev, vector<vector<int>>& nbl, vector<bool>& visited)
+using pii = pair<int, int>;
+
+int r, c;
+vector<vector<char>> grid;
+
+bool on_grid(int i, int j)
 {
-    if (visited[i] == true) return true;
-    bool has_loop = false;
-    visited[i] = true;
-    for (int j : nbl[i])
-    {
-        if (j != prev)
-        {
-            has_loop |= dfs(j, i, nbl, visited);
-        }
-    }
-    return has_loop;
+    if (i < 0 || i >= r) return false;
+    if (j < 0 || j >= c) return false;
+    return true;
 }
 
-int main()
+bool is_road(int i, int j)
 {
-    int n, m; cin >> n >> m;
-    vector<vector<int>> nbl(n);
-    rep(i, m)
+    if (on_grid(i, j) == false) return false;
+    if (grid[i][j] == '#') return false;
+    return true;
+}
+
+void enq(queue<pii>& q, int i, int j)
+{
+    rep(k, 4)
     {
-        int u, v; cin >> u >> v;
-        nbl[u-1].push_back(v-1);
-        nbl[v-1].push_back(u-1);
-    }
-    vector<bool> visited(n, false);
-    int count = 0;
-    rep(i, n)
-    {
-        if (visited[i] == false)
+        if (is_road(i+di[k], j+dj[k]))
         {
-            bool has_loop = dfs(i, -1, nbl, visited);
-            if (has_loop == false) count++;
+            q.push(pii(i+di[k], j+dj[k]));
+            grid[i+di[k]][j+dj[k]] = '#';
         }
     }
-    cout << count << endl;
+}
+int main()
+{
+    cin >> r >> c;
+    int si, sj; cin >> si >> sj; si--; sj--;
+    int gi, gj; cin >> gi >> gj; gi--; gj--;
+    grid = vector<vector<char>>(r, vector<char>(c));
+    rep(i, r)rep(j, c)
+    {
+        char ch; cin >> ch;
+        grid[i][j] = ch;
+    }
+    queue<pii> q;
+    grid[si][sj] = '#';
+    pii start = pii(si, sj);
+    q.push(start);
+    int count = 0;
+    while(!q.empty())
+    {
+        queue<pii> q2;
+        while(!q.empty())
+        {
+            pii p = q.front(); q.pop();
+            if (p.fi == gi && p.se == gj)
+            {
+                println(count);
+                return 0;
+            }
+            q2.push(p);
+        }
+        while(!q2.empty())
+        {
+            pii p = q2.front(); q2.pop();
+            enq(q, p.fi, p.se);
+        }
+        count++;
+    }
 }
