147436e9be0444e46edc07e87719cc5d3fd854f2
kjakx <fxhfr163@ybb.ne.jp>
2020-05-02 09:26:19 +0000

    cheese

--- playground.cpp
+++ playground.cpp
@@ -15,68 +15,58 @@ const int dj[8] = {0, 1, 0, -1, -1, 1, 1, -1};
 
 using pii = pair<int, int>;
 
-int r, c;
-vector<vector<char>> grid;
+int h, w;
 
 bool on_grid(int i, int j)
 {
-    if (i < 0 || i >= r) return false;
-    if (j < 0 || j >= c) return false;
+    if (i < 0 || i >= h) return false;
+    if (j < 0 || j >= w) return false;
     return true;
 }
 
-bool is_road(int i, int j)
+bool is_road(int i, int j, vector<vector<char>>& grid)
 {
     if (on_grid(i, j) == false) return false;
-    if (grid[i][j] == '#') return false;
+    if (grid[i][j] == 'X') return false;
     return true;
 }
 
-void enq(queue<pii>& q, int i, int j)
-{
-    rep(k, 4)
-    {
-        if (is_road(i+di[k], j+dj[k]))
-        {
-            q.push(pii(i+di[k], j+dj[k]));
-            grid[i+di[k]][j+dj[k]] = '#';
-        }
-    }
-}
 int main()
 {
-    cin >> r >> c;
-    int si, sj; cin >> si >> sj; si--; sj--;
-    int gi, gj; cin >> gi >> gj; gi--; gj--;
-    grid = vector<vector<char>>(r, vector<char>(c));
-    rep(i, r)rep(j, c)
+    int n;
+    cin >> h >> w >> n;
+    pii s;
+    vector<pii> gv(n);
+    vector<vector<char>> grid(h, vector<char>(w));
+    vector<vector<int>> t(h, vector<int>(w, -1));
+    rep(i, h)rep(j, w)
     {
         char ch; cin >> ch;
+        if (isdigit(ch)) gv[ch - '0' - 1] = pii(i, j);
+        else if (ch == 'S') { s = pii(i, j); }
         grid[i][j] = ch;
     }
-    queue<pii> q;
-    grid[si][sj] = '#';
-    pii start = pii(si, sj);
-    q.push(start);
-    int count = 0;
-    while(!q.empty())
+    int cnt = 0;
+    for (pii g : gv)
     {
-        queue<pii> q2;
-        while(!q.empty())
+        queue<pii> q; q.push(s);
+        vector<vector<int>> tm = t;
+        tm[s.fi][s.se] = 0; 
+        while (!q.empty())
         {
             pii p = q.front(); q.pop();
-            if (p.fi == gi && p.se == gj)
+            if (p == g) break;
+            rep(k, 4)
             {
-                println(count);
-                return 0;
+                if (is_road(p.fi+di[k], p.se+dj[k], grid) && tm[p.fi+di[k]][p.se+dj[k]] == -1)
+                {
+                    q.push(pii(p.fi+di[k], p.se+dj[k]));
+                    tm[p.fi+di[k]][p.se+dj[k]] = tm[p.fi][p.se] + 1;
+                }
             }
-            q2.push(p);
         }
-        while(!q2.empty())
-        {
-            pii p = q2.front(); q2.pop();
-            enq(q, p.fi, p.se);
-        }
-        count++;
+        cnt += tm[g.fi][g.se];
+        s = pii(g.fi, g.se);
     }
+    cout << cnt << endl;
 }
