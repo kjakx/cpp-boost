8f231efff654605a5f20337bbffcabb9fc23bd24
kjakx <k.jak.xg0g11@gmail.com>
2020-07-12 10:47:14 +0000

    how many islands

--- code.cpp
+++ code.cpp
@@ -48,42 +48,40 @@ using pdd = pair<double, double>;
 #include <vector>
 using namespace std;
 
-void dfs(int n, int &t, vector<vector<int>>& g, vector<int>& d, vector<int>& f, vector<bool>& seen)
+bool ongrid(int h, int w, vector< vector<int> >& g)
 {
-    t++;
-    if (d[n] == 0) d[n] = t;
-    seen[n] = true;
-    for (int i = 0; i < g[n].size(); i++)
+    if (h >= 0 && h < g.size() && w >= 0 && w < g[0].size()) return true;
+    return false;
+}
+
+bool dfs(int h, int w, vector< vector<int> >& g, vector< vector<bool> >& seen)
+{
+    seen[h][w] = true;
+    if (g[h][w] == 0) return false;
+    for (int i = 0; i < 8; i++)
     {
-        if (seen[g[n][i]]) continue;
-        dfs(g[n][i], t, g, d, f, seen);
+        if (ongrid(h+di[i], w+dj[i], g) && seen[h+di[i]][w+dj[i]] == false)
+            dfs(h+di[i], w+dj[i], g, seen);
     }
-    t++;
-    f[n] = t;
+    return true;
 }
 
 int main()
 {
-    int n; cin >> n;
-    vector<vector<int>> g(n, vector<int>());
-    for (int i = 0; i < n; i++)
+    while (true)
     {
-        int u, k; cin >> u >> k; u--;
-        for (int j = 0; j < k; j++)
-        {
-            int v; cin >> v; v--;
-            g[u].push_back(v);
-        }
-    } 
-    vector<int> d(n, 0);
-    vector<int> f(n, 0);
-    vector<bool> seen(n, false);
-    int t = 0;
-    for (int i = 0; i < n; i++)
-        if (seen[i] == false)
-            dfs(i, t, g, d, f, seen);
-    for (int i = 0; i < n; i++)
-    {
-        cout << i + 1 << " " <<  d[i] << " " << f[i] << endl;
+        int w, h; cin >> w >> h;
+        if (w == 0 && h == 0) break;
+        vector< vector<int> > g(h, vector<int>(w));
+        for (int i = 0; i < h; i++)
+            for (int j = 0; j < w; j++)
+                { int c; cin >> c; g[i][j] = c;}
+        vector< vector<bool> > seen(h, vector<bool>(w, false));
+        int cnt = 0;
+        for (int i = 0; i < h; i++)
+            for (int j = 0; j < w; j++)
+                if (seen[i][j] == false)
+                    if (dfs(i, j, g, seen) == true) cnt++;
+        cout << cnt << endl;
     }
 }
