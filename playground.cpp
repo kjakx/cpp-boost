e05094625f40e797443b00012fe1f41cd4a4a1a3
kjakx <k.jak.xg0g11@gmail.com>
2020-07-22 04:38:55 +0000

    knapsack

--- playground.cpp
+++ playground.cpp
@@ -1,25 +1,19 @@
-#include "header.h"
-
+//#include "header.h"
+#include <iostream>
+#include <vector>
+using namespace std;
 int main()
 {
-    int n, wm; cin >> n >> wm;
-    vector<int> v(n);
-    vector<int> w(n);
-    for (int i = 0; i < n; i++)
-    {
+    int N, W; cin >> N >> W;
+    vector<int> v(N), w(N);
+    for (int i = 0; i < N; i++)
         cin >> v[i] >> w[i];
-    }
-    vector< vector<int> > dp(n+1, vector<int>(wm+1, 0));
-    for (int i = 0; i < n; i++)
-    {
-        for (int wi = 0; wi <= wm; wi++)
-        {
-            if (wi >= w[i])
-                dp[i+1][wi] = max(dp[i][wi - w[i]] + v[i], dp[i][wi]);
-            else
-                dp[i+1][wi] = dp[i][wi];
-        }
-    }
-    cout << dp[n][wm] << endl;
+    vector<int> dp(W+1, 0);
+    for (int i = 1; i <= W; i++)
+        for (int j = 0; j < N; j++)
+            if (w[j] <= i)
+                dp[i] = max(dp[i-w[j]]+v[j], dp[i]);
+
+    cout << dp[W] << endl;
     return 0;
 }
