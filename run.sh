c42e1536a371ba63597b771c8a1d39779949dd51
kjakx <43692824+kjakx@users.noreply.github.com>
2020-11-19 10:40:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cat header.h playground.cpp | sed '/#include "header.h"/d' > code.cpp
-g++ -o code code.cpp && echo "./code" && ./code
+g++ -o code code.cpp && echo -I./ac-library/ "./code" && ./code
